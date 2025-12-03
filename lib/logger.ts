cf0c82048cca27b96c0991fca0ae5a53bf521cc2
Rúben Silver <drogadinhosilver@gmail.com>
2025-12-03 21:34:23 +0100

    Update logger.ts

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,1 +1,12 @@
-export function log(level:string, message:string, meta?:any){ console.log(`[${level}] ${message}`, meta||'') }
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function log(level: "INFO" | "WARN" | "ERROR", message: string, meta?: any) {
+  try {
+    await prisma.log.create({
+      data: { level, message, meta: meta || {} },
+    });
+  } catch (e) {
+    console.error("log error", e);
+  }
+}
