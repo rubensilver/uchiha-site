a6fb22bd736e83e04cea89f3abb612d94963c26c
Rúben Silver <drogadinhosilver@gmail.com>
2025-12-04 14:38:24 +0100

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,12 +1,11 @@
+import { ReactNode } from "react";
 import Sidebar from "@/components/Sidebar";
 
-export default function AdminLayout({ children }) {
+export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen flex bg-black text-white">
+    <div className="min-h-screen bg-black text-white flex">
       <Sidebar />
-      <main className="flex-1 p-6 ml-0">
-        {children}
-      </main>
+      <div className="flex-1">{children}</div>
     </div>
   );
 }
