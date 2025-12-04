806ed9da2b28906b2f81a3c1ecb324ba4c86af09
Rúben Silver <drogadinhosilver@gmail.com>
2025-12-04 23:22:24 +0100

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,14 +1,15 @@
 // app/admin/layout.tsx
+import React from "react";
 import Header from "@/components/Header";
 
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
+export default function AdminLayout(
+  { children }: { children: React.ReactNode }
+) {
   return (
     <div className="min-h-screen bg-[var(--bg)] text-[var(--text)]">
-      {/* Navbar fixa */}
       <Header />
-
-      {/* Conteúdo do painel */}
-      <main className="max-w-5xl mx-auto px-4 py-6">
+      
+      <main className="max-w-5xl mx-auto p-4">
         {children}
       </main>
     </div>
