ee264949bad590b3284f628973f04d138f26bbd9
Rúben Silver <drogadinhosilver@gmail.com>
2025-12-04 13:09:35 +0100

    Create ui.ts

---
+++ lib/ui.ts
@@ -0,0 +1,9 @@
+// small helper: write sharingan svg to /public/sharingan-small.svg or import as data URI
+export const sharinganSvg = `<?xml version="1.0" encoding="UTF-8"?>
+<svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="32" cy="32" r="30" fill="#7b0000"/>
+  <circle cx="32" cy="32" r="16" fill="#000"/>
+  <g fill="#ff4d4d">
+    <path d="M16 32a16 16 0 0 1 32 0 16 16 0 0 1-32 0z"/>
+  </g>
+</svg>`;
