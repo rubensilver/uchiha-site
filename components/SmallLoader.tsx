9c6aedd1090842cbbd4bef1de0474428923ffe92
Rúben Silver <drogadinhosilver@gmail.com>
2025-12-05 18:29:51 +0100

    Create SmallLoader.tsx

---
+++ components/SmallLoader.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function SmallLoader() {
+  return (
+    <div className="inline-block">
+      <img
+        src="/sharingan-small.svg"
+        className="w-5 h-5 animate-spin"
+        alt="loading"
+      />
+    </div>
+  );
+}
