320796a9b37e4dd1afd4a13d62d2a7a181af0bab
Rúben Silver <drogadinhosilver@gmail.com>
2025-12-05 09:43:16 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,16 +4,18 @@ import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
+
+  // cookie onde guardamos a autenticação
   const session = req.cookies.get("session_token")?.value;
 
-  // Rotas que não exigem login
+  // rotas públicas
   const publicRoutes = ["/admin/login", "/admin/register"];
 
   const isPublic = publicRoutes.some((route) => url.pathname.startsWith(route));
-  const isAdminRoute = url.pathname.startsWith("/admin");
+  const isAdmin = url.pathname.startsWith("/admin");
 
-  // Se for rota admin e não for pública → precisa de login
-  if (isAdminRoute && !isPublic) {
+  // Proteção das rotas
+  if (isAdmin && !isPublic) {
     if (!session) {
       url.pathname = "/admin/login";
       return NextResponse.redirect(url);
@@ -24,7 +26,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    "/admin/:path*", // aplica middleware em todo /admin
-  ],
+  matcher: ["/admin/:path*"],
 };
