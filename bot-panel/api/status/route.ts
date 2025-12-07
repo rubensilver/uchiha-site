2dadb19609c56a01fdce963e0d842d53f865910a
Rúben Silver <drogadinhosilver@gmail.com>
2025-12-07 16:33:51 +0100

    Update route.ts

--- bot-panel/api/status/route.ts
+++ bot-panel/api/status/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { verifyAuth } from "@/utils/auth";
+import { verifyAuth } from '../../../utils/auth';
 
 export async function GET(req: Request) {
   const auth = verifyAuth(req.headers);
