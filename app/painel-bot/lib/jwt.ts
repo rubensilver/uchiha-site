de21063194abe11f98a035396f8c2321139fb52a
Rúben Silver <drogadinhosilver@gmail.com>
2025-12-07 19:29:30 +0100

    Delete app/painel-bot/lib directory

--- app/painel-bot/lib/jwt.ts
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export function verifyAuth(token: string) {
-  try {
-    return jwt.verify(token, process.env.SECRET_KEY || "");
-  } catch (e) {
-    return null;
-  }
-}
