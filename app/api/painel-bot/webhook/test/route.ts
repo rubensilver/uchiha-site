6bf306f8559527d5ba597593af5db38c8d01540c
Rúben Silver <drogadinhosilver@gmail.com>
2025-12-08 18:25:20 +0100

    Update route.ts

--- app/api/painel-bot/webhook/test/route.ts
+++ app/api/painel-bot/webhook/test/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from 'next/server';
+
 export async function POST(){ 
   console.log('Webhook test received');
-  return NextResponse.text('Evento de teste recebido no servidor');
+  return new Response('Evento de teste recebido no servidor', {
+    status: 200
+  });
 }
